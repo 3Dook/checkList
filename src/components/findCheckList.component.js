8960ca4589d69d6eb2fbe4a494959580f5a9eabc
doandu <doandu@oregonstate.edu>
2020-09-13 15:30:28 -0700

    small edits to start front end

--- src/components/findCheckList.component.js
+++ src/components/findCheckList.component.js
@@ -5,28 +5,28 @@ import axios from 'axios';
 //import checkList from '../../backEnd/models/checkList';
 
 
-  const CheckListHolder = props => (
-    <div>
-      <table className="table  table-bordered">
-        <tbody>
-          <tr>
-            <th>TITLE</th>
-            <td>
-              {props.check.title}
-            </td>
-          </tr>
-          <tr>
-            <th>DESCRIPTION</th>
-            <td>
-              {props.check.description}
-            </td>
-          </tr>
-          <tr>
-            <td colSpan='2'><Link to={"/edit/"+props.check._id}>VIEW</Link></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+const CheckListHolder = props => (
+  <div>
+    <table className="table  table-bordered">
+      <tbody>
+        <tr>
+          <th>TITLE</th>
+          <td>
+            {props.check.title}
+          </td>
+        </tr>
+        <tr>
+          <th>DESCRIPTION</th>
+          <td>
+            {props.check.description}
+          </td>
+        </tr>
+        <tr>
+          <td colSpan='2'><Link to={"/edit/"+props.check._id}>VIEW</Link></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 )
 
 export default class findCheckList extends Component {
--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,13 +1,110 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+//import checkList from '../../backEnd/models/checkList';
 
 
+const CheckListHolder = props => (
+  <div>
+    <table className="table  table-bordered">
+      <tbody>
+        <tr>
+          <th>TITLE</th>
+          <td>
+            {props.check.title}
+          </td>
+        </tr>
+        <tr>
+          <th>DESCRIPTION</th>
+          <td>
+            {props.check.description}
+          </td>
+        </tr>
+        <tr>
+          <td colSpan='2'><Link to={"/edit/"+props.check._id}>VIEW</Link></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+)
+
+// eslint-disable-next-line
+/*
+const checkListColumn = props => (
+  <div>
+    <table className="table table-bordered">
+      <thead></thead>
+      <tbody>
+      <tr>
+          <th>TITLE</th>
+          <td>
+            {props.check.title}
+          </td>
+        </tr>
+        <tr>
+          <th>DESCRIPTION</th>
+          <td>
+            {props.check.description}
+          </td>
+        </tr>
+        <tr>
+          <td colSpan='2'><Link to={"/edit/"+props.check._id}>VIEW</Link></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+)
+*/
 export default class home extends Component {
+  constructor(props){
+    super(props);
 
+    this.state={
+      checkList: [],
+      column: {
+        title: '',
+        description: '',
+        itemList: [],
+        tags: []
+      }
+    };
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:5000/api/all')
+      .then(response =>{
+        this.setState({checkList: response.data})
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  contentTable(){
+    return this.state.checkList.map(idx =>{
+      return (
+      <div onClick={() => this.handleContent(idx._id)} value={idx._id} key={idx._id}>
+        <CheckListHolder check={idx} key={idx._id}/>
+      </div>
+      )
+    })
+  }
+
+  handleContent(event, idx){
+    //onsole.log(event.target.value) //undefined
+    console.log(event)
+  }
   render() {
     return (
+      <div> 
         <div>
-            <p>You are in home</p>
+          <h1>HOME</h1>
+          <p>Welcome to a CRUD APP for managing checklist.</p>
         </div>
+        <div >
+            {this.contentTable()}
+        </div>
+      </div>
     );
   }
 }
